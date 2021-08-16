44774fc7984ba20bf0f08f13946bf5be50d297e5
phamnhutnpx <xuannhutzz@gmail.com>
2021-08-16 23:51:29 +0700

    add love

---
+++ love/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'He Lu chào cậu nhé!',
+    introDesc: 'Tớ có điều này muốn hỏi Thêm nhớ phải trả lời thật lòng nhaaa',
+    btnIntro: 'hihi',
+    title: 'Có thích Tớ đúng hông nào😊',
+    desc: 'Ko trả lời mà thoát ra tức là yêu tui á nhá😁',
+    btnYes: 'Thích rất rất nhiềuuu🥰🥰🥰',
+    btnNo: 'Không nhé!!! :3',
+    question: 'Lí do thích Tớ là gi nèe <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì cậu đẹp try vllll😍',
+    mess: 'Tớ biết màa^^ Yêu cậu😘😘',
+    messDesc: 'Nhớ giữ gìn sức khoẻ ăn uống đầy đủ đúng giờ giấc nhe. Hông là trở bệnh á ở 1 mình hông ai chăm đc đâu. Hihi ❤',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/me'
+}
